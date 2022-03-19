1f87c8db7ae319de784b0f9a71c1e49d59f526f6
SuhanaCode <87379808+SuhanaCode@users.noreply.github.com>
2022-03-19 17:46:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,30 @@ function preload(){
 function setup(){
   canvas = createCanvas(350 , 350);
   canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300 , 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video , modelLoaded);
+  poseNet.on('poses' , gotPoses);
 }
 
-function draw(){
+function gotPoses(results){
+  if(results.length > 0){
+    console.log(results);
+    console.log("Nose X =" + results[0].pose.nose.x);
+    console.log("Nose X =" + Results[0].pose.nose.y);
+  }
+  }
+
+function modelLoaded(){
+  console.log("PoseNet is Intilized.");
 }
 
+  function draw(){
+    image(video , 0 , 0 , 350 , 350);
+  }
+
+  function take_snapshot(){
+    save('hereFilter.png')
+  }
